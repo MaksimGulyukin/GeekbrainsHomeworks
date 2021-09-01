063854175dcd27e128d645fc2aae4d3ae0c3c686
Maksim <maksim.gulukin@gmail.com>
2021-09-01 14:13:03 +0300

    Довабил первое дз

---
+++ Homework1/Lesson1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Привет " + name + "!" + "Сегодня: " + DateTime.Now);
+
+            Console.ReadKey();
+        }
+    }
+}
