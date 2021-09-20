a1ac16a755b9e728055987402788487e627cb063
Maksim <maksim.gulukin@gmail.com>
2021-09-20 15:12:49 +0300

    добавил дз к 5 уроку

---
+++ ДЗ к 5 уроку задание 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ДЗ_к_5_уроку_2_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "startup.txt";
+
+            File.AppendAllText(fileName, DateTime.Now.ToString("HH:mm:ss ;"));
+            string readFile =File.ReadAllText(fileName);
+            Console.WriteLine(readFile);
+        }
+    }
+}
